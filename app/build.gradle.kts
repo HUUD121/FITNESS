baef0fdcbbdb5c5c57ed1298ea28169b9bd6bd3f
HUUD121 <hudomohamed7@gmail.com>
2024-01-04 14:23:40 +0800

    ab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,10 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
 }
